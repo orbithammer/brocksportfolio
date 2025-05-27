e44c3ee4fc5fb0c4da97801dbb177d2e03d5e860
Brock Penner <orbithammer@users.noreply.github.com>
2025-05-27 14:00:15 +0900

    Update page.tsx

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -4,9 +4,9 @@ import { getProjectBySlug } from "@/app/lib/projects"
 import GithubIcon from "@/public/icons/github.svg"
 
 type ProjectPageProps = {
-  params: {
+  params: Promise<{
     slug: string
-  }
+  }>
 }
 
 export default async function ProjectPage({ params }: ProjectPageProps) {
